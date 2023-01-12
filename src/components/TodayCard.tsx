19375cf39df1cf40ab8971f232b5a2ceb03b6655
Donovan <donovanjuniogomez@gmail.com>
2023-01-12 00:05:39 -0800

    style today card

--- src/components/TodayCard.tsx
+++ src/components/TodayCard.tsx
@@ -6,9 +6,9 @@ interface TodayCardProps {
 
 const TodayCard: React.FC<TodayCardProps> = ({ today }) => {
   return (
-    <div>
-      <h2>Hi Adventurer!</h2>
-      <h1>It's {today}</h1>
+    <div className="grid w-5/6 place-items-center border-2 p-4">
+      <h2 className="text-4xl">Hi Adventurer!</h2>
+      <h1 className="text-6xl">It's {today}</h1>
     </div>
   );
 };
